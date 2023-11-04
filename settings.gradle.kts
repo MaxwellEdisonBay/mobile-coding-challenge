e78774724abc9fef3ab67c8024770980e09747f7
Ivan <ivanteslenkopraha@gmail.com>
2023-11-04 13:43:47 -0400

    Implemented project module structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "Podcast App"
 include(":app")
- 
+include(":feature-dashboard")
+include(":feature-details")
+include(":core")
+include(":core-networking")
+include(":core-caching")
+include(":core-ui")
