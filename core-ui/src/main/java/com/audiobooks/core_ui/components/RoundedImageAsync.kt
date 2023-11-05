3798e8a9007a35d6367966ea9cb15e1d7bd9000a
Ivan <ivanteslenkopraha@gmail.com>
2023-11-05 16:57:07 -0500

    Added async image placeholder for better preview

--- core-ui/src/main/java/com/audiobooks/core_ui/components/RoundedImageAsync.kt
+++ core-ui/src/main/java/com/audiobooks/core_ui/components/RoundedImageAsync.kt
@@ -4,18 +4,24 @@ import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.audiobooks.core_ui.R
 import com.audiobooks.core_ui.theme.PodcastAppTheme
 
 @Composable
 fun RoundedImageAsync(
-    url : String,
+    url: String,
     @StringRes contentDescRes: Int,
     modifier: Modifier = Modifier
 ) {
     AsyncImage(
-        model = url,
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(url)
+            .placeholder(R.drawable.placeholder)
+            .build(),
         contentDescription = stringResource(id = contentDescRes),
         modifier = modifier.clip(PodcastAppTheme.shapes.mediumCornerShape)
     )
