87597f69d4caa0aa96451b3b23308ddd49fdc12d
Ivan <ivanteslenkopraha@gmail.com>
2023-11-06 06:00:34 -0500

    Added navigation graph animation

--- feature-podcasts/src/main/java/com/audiobooks/podcasts/navigation/PodcastsNavGraph.kt
+++ feature-podcasts/src/main/java/com/audiobooks/podcasts/navigation/PodcastsNavGraph.kt
@@ -6,6 +6,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.audiobooks.core_ui.navigation.BottomNavGraphs
 import com.audiobooks.core_ui.navigation.parentViewModel
+import com.audiobooks.core_ui.util.enterTransition
+import com.audiobooks.core_ui.util.exitTransition
+import com.audiobooks.core_ui.util.popEnterTransition
+import com.audiobooks.core_ui.util.popExitTransition
 import com.audiobooks.podcasts.SharedViewModel
 import com.audiobooks.podcasts.details.DetailsScreen
 import com.audiobooks.podcasts.landing.LandingScreen
@@ -15,11 +19,24 @@ fun NavGraphBuilder.injectPodcastsNavGraph(navController: NavController) {
         route = BottomNavGraphs.Podcasts.route,
         startDestination = PodcastsRoutes.Landing.route
     ) {
-        composable(route = PodcastsRoutes.Landing.route) {
+        composable(route = PodcastsRoutes.Landing.route,
+            enterTransition = {
+                enterTransition
+            },
+            exitTransition = {
+                exitTransition
+            },
+            popEnterTransition = {
+                popEnterTransition
+            }
+        ) {
             val sharedViewModel: SharedViewModel = it.parentViewModel(navController = navController)
             LandingScreen(navController, sharedViewModel)
         }
-        composable(route = PodcastsRoutes.Details.route) {
+        composable(route = PodcastsRoutes.Details.route,
+            enterTransition = { enterTransition },
+            popExitTransition = { popExitTransition }
+        ) {
             val sharedViewModel: SharedViewModel = it.parentViewModel(navController = navController)
             DetailsScreen(navController, sharedViewModel)
         }
