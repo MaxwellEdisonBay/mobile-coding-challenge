ed2f69c79d66c44882331562f7a3b7b27cd26f5d
Ivan <ivanteslenkopraha@gmail.com>
2023-11-06 04:01:45 -0500

    Added HTML text for DetailsScreen

--- feature-podcasts/src/main/java/com/audiobooks/podcasts/details/DetailsScreen.kt
+++ feature-podcasts/src/main/java/com/audiobooks/podcasts/details/DetailsScreen.kt
@@ -1,11 +1,17 @@
 package com.audiobooks.podcasts.details
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.audiobooks.core_ui.components.BaseScaffold
 import com.audiobooks.core_ui.components.RoundedImageAsync
+import com.audiobooks.core_ui.components.text.HtmlText
 import com.audiobooks.dashboard.R
 import com.audiobooks.podcasts.SharedViewModel
 
@@ -18,14 +24,20 @@ internal fun DetailsScreen(
     val state = viewModel.detailsState.value
     val podcast = sharedViewModel.selectedPodcast
     BaseScaffold(navController = navController) {
-        podcast?.let {
-            Text(text = podcast.title)
-            Text(text = podcast.publisher)
-            RoundedImageAsync(
-                url = podcast.largeImageUrl,
-                contentDescRes = R.string.feature_podcast_details_large_image_alt
-            )
-            Text(text = podcast.description)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            podcast?.let {
+                Text(text = podcast.title)
+                Text(text = podcast.publisher)
+                RoundedImageAsync(
+                    url = podcast.largeImageUrl,
+                    contentDescRes = R.string.feature_podcast_details_large_image_alt
+                )
+                HtmlText(html = podcast.description)
+            }
         }
     }
 }
